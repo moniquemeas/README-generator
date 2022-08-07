bb115cd7d07a5606e13bf248d049590af57f5487
Monique <monique.meas@gmail.com>
2022-08-07 15:45:27 -0400

    add page-template

--- index.js
+++ index.js
@@ -1,74 +1,101 @@
 const inquirer = require('inquirer');
-const fs = require ('fs');
+const fs = require('fs');
+const generatePage = require('./utils/page-template.js');
+
+
+
 
 const userInput = () => {
-return inquirer.prompt([
+  
+  return inquirer.prompt([
     {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username? (Required)' ,
-        validate: githubInput => {
-            if (githubInput) {
-              return true;
-            } else {
-              console.log('Please enter your GitHub username!');
-              return false;
-            }
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your application? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('You need to enter an application name!');
+          return false;
+        }
+      }
+    },
+    {
+      type:'input',
+      name: 'fileName',
+      message: 'What do you want to name this file? (Required)',
+      validate: fileNameInput => {
+        if (fileNameInput) {
+          return true;
+        } else {
+          console.log('You need to enter a file name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username? (Required)' ,
+      validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
           }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your Email address?',
-        validate: emailInput => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project name!');
-              return false;
-            }
-          }
-    },
-    {
-        type: 'input',
-        name: 'project',
-        message: "What is your project's name? (Required)",
-        validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project name!');
-              return false;
-            }
-          }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: "Provide a description of the project"
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: "What kind of license should your projoect have?"
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: "How do you install this application?"
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: "How do you test this appliction?"
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: "How do you use this application?"
-    },
+        }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Provide a description of the project:"
+},
+{
+  type: 'input',
+  name: 'install',
+  message: "How do you install this application?"
+},
+{
+  type: 'input',
+  name: 'usage',
+  message: "How do you use this application?"
+},
+{
+  type: 'input',
+  name: 'test',
+  message: "How do you test this appliction?"
+},
+{
+  type: 'list',
+  name: 'license',
+  message: 'Select licenses for your application:',
+  choices: ['Apache_2.0', 'IPL-1.0', 'MIT', 'ISC' ],
+},
+{
+  type: 'input',
+  name: 'contributor',
+  message: "Please enter the contributor's name:"
+},
+{
+  type: 'input',
+  name: 'contact',
+  message: "What is your email address?"
+},
 
-    
 ]);
 };
-userInput().then(answers => console.log(answers));
+//userInput().then(answers => console.log(answers));
+//console.log(generatePage(profileDataArgs));
+
+function writeTofile(fileName, data){
+fs.appendFile(`${fileName}.md`, data, 
+(err) => err ? console.error(err) : console.log('Your README.md is generated.')
+)}
+
+async function getData() {
+  let answers = await userInput();
+  writeTofile((answers.fileName), (generatePage(answers)))
+}
+getData();
---
+++ utils/page-template.js
@@ -0,0 +1,48 @@
+const generatePage = (data) => {
+
+ 
+    return `
+    # Preoject Name: ${data.name}
+
+
+    # Badges: 
+    [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+
+    # GitHub username: ${data.username}
+    
+    # Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Test](#test)
+    * [Contributing](#contributing)
+    * [License](#license)
+    * [Contact](#contact)
+
+    # Description:
+      ${data.description}
+    
+    # Installation
+      ${data.install}
+    
+    # Usage
+      ${data.usage}
+    
+    # Test:
+      ${data.test}
+    
+    # Contributing:
+      ${data.contributor}
+    
+    # License:
+      ${data.license}
+      To find out more about ${data.license} go to (#https://opensource.org/licenses/${data.license})
+    
+    # Contact:
+      ${data.contact}
+
+    `
+  
+  }
+  module.exports = generatePage;
